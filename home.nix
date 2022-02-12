1957092117fbc0bb311f9cdd19f96297f2e40822
emssej <emssej@gmail.com>
2022-02-12 21:16:10 +0100

    More programs

--- home.nix
+++ home.nix
@@ -26,7 +26,9 @@
 	};
 
 	xdg.configFile = {
+		"audacity/audacity.cfg".source = ./config/audacity;
 		"cros-garcon.conf".source = ./config/garcon;
+		"inkscape/preferences.xml".source = ./config/inkscape;
 		"systemd/user/cros-garcon.service.d/override.conf".source = ./config/garcon-service;
 		"weston.ini".source = ./config/weston;
 	};
